484b677f79a2ce9f805d931c038a29b43746e086
Zulham Ari <ari.comunity1@gmail.com>
2023-05-10 23:42:10 +0700

    change data structure  when fetching

---
+++ JS/src/utils/data-api.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://brainy-rose-rugby-shirt.cyclic.app/v1";
+
+async function totalPageProv() {
+  const response = await fetch(`${BASE_URL}/get-prov-page`);
+  const responseJson = await response.json();
+
+  return { error: false, data: responseJson };
+}
+
+async function getProvData({ pageNumber }) {
+  const response = await fetch(
+    `${BASE_URL}get-province-all?page=${pageNumber}`
+  );
+  const responseJson = await response.json();
+
+  return { error: false, data: responseJson };
+}
+
+export { totalPageProv, getProvData };

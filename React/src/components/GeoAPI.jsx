d5e068504fbb30abf54d7bfe676c3c2b1c21a02a
Zulham Ari <ari.comunity1@gmail.com>
2023-05-23 12:02:54 +0700

    update tileLayer

--- React/src/components/GeoAPI.jsx
+++ React/src/components/GeoAPI.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getProvData, totalPageProv } from "../utils/data-api";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
 export default function GeoAPI() {
   const [getData, setData] = useState(null);
@@ -26,7 +27,16 @@ export default function GeoAPI() {
 
   return (
     <div>
-      <p>P</p>
+      <MapContainer
+        center={[-1.2480891, 115.419]}
+        zoom={5}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }

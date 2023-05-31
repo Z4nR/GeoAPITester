18e739d2123cbeda0c554370c23b4de8074a2e07
Zulham Ari <ari.comunity1@gmail.com>
2023-05-31 19:57:07 +0700

    finish one trouble still have another problem

--- React/src/components/GeoAPI.jsx
+++ React/src/components/GeoAPI.jsx
@@ -14,46 +14,57 @@ export default function GeoAPI() {
       setTotalPage(total);
     });
 
-    let i = 1;
-    let batchSize = 3;
-    while (i < getTotalPage) {
-      let dataSize = batchSize;
-      if (i + batchSize > getTotalPage) {
-        dataSize = getTotalPage - i + 1;
+    const loadData = async () => {
+      let i = 1;
+      let batchSize = 5;
+      const mixData = [];
+      while (i < getTotalPage) {
+        let dataSize = batchSize;
+        if (i + batchSize > getTotalPage) {
+          dataSize = getTotalPage - i + 1;
+        }
+
+        const fetchData = async () => {
+          const data = await Promise.all(
+            Array(dataSize)
+              .fill(null)
+              .map((_, index) =>
+                getProvData(i + index).then((data) => {
+                  return data.data;
+                })
+              )
+          );
+          mixData.push(...data);
+        };
+
+        await fetchData();
+        i += dataSize;
       }
 
-      const fetchData = async () => {
-        const data = await Promise.all(
-          Array(dataSize)
-            .fill(null)
-            .map((_, index) =>
-              getProvData(i + index).then((data) => {
-                return data.data;
-              })
-            )
-        );
-        return data;
-      };
+      const mergeArray = mixData.flat();
 
-      console.log(fetchData());
-      i += dataSize;
-    }
+      setDataProv(mergeArray);
+    };
+
+    loadData();
+  }, [getTotalPage]);
+
+  useEffect(() => {
+    console.log(getDataProv);
 
     if (getDataProv !== null) {
       getDataProv.find((d) => {
-        console.log(d);
         const feature = d.provFeature;
+        setFeature(feature);
       });
     }
-  }, [getTotalPage]);
+  }, [getDataProv]);
+
+  console.log(getFeature);
 
   return (
     <div>
-      <MapContainer
-        center={[-1.2480891, 115.419]}
-        zoom={5}
-        scrollWheelZoom={true}
-      >
+      <MapContainer center={[-1.2480891, 122]} zoom={5} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
